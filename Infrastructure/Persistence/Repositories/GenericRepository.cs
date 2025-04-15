9fd755d6facd7fd6c627708bde10d5dcff8dfd84
El3zab <ahmed.3zab99@gmail.com>
2025-04-15 22:26:05 +0200

    Create ProductController That Have Four Operations To Get [Products - Brands - Types]  Data

--- Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -22,9 +22,15 @@ namespace Persistence.Repositories
 
         public async Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges = false)
         {
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return trackChanges ?
+                 await _context.Products.Include(P => P.ProductType).Include(P => P.ProductBrand).ToListAsync() as IEnumerable<TEntity>
+                : await _context.Products.Include(P => P.ProductType).Include(P => P.ProductBrand).AsNoTracking().ToListAsync() as IEnumerable<TEntity>;
+            }
             return trackChanges ?
                  await _context.Set<TEntity>().ToListAsync()
-                :await _context.Set<TEntity>().AsNoTracking().ToListAsync()
+                : await _context.Set<TEntity>().AsNoTracking().ToListAsync();
 
             //if(trackChanges) return await _context.Set<TEntity>().ToListAsync();
             //return await _context.Set<TEntity>().AsNoTracking().ToListAsync();
@@ -32,6 +38,10 @@ namespace Persistence.Repositories
 
         public async Task<TEntity?> GetAsync(TKey id)
         {
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return await _context.Products.Include(P => P.ProductType).Include(P => P.ProductBrand).FirstOrDefaultAsync(P => P.Id == id as int?) as TEntity;
+            }
             return await _context.Set<TEntity>().FindAsync(id);
         }
 
