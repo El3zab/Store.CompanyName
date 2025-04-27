b37e9918d71e41069dfcbb10e29ff27fe789b042
El3zab <ahmed.3zab99@gmail.com>
2025-04-28 00:17:27 +0300

    Implement Basket Service [DL(Redis)-BLL-PL]

--- Core/Services.Abstraction/IServicesManager.cs
+++ Core/Services.Abstraction/IServicesManager.cs
@@ -9,5 +9,8 @@ namespace Services.Abstraction
     public interface IServicesManager
     {
         IProductService ProductService { get; }
+
+        IBasketService BasketService { get; }
+
     }
 }
---
+++ Infrastructure/Presentation/BasketsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketsController(IServicesManager serviceManager) : ControllerBase
+    {
+        [HttpGet] // GET: /api/baskets?id=jbj
+        public async Task<IActionResult> GetBasketById(string id)
+        {
+            var result = await serviceManager.BasketService.GetBasketAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost] // POST: /api/baskets
+        public async Task<IActionResult> UpdateBasket(BasketDto basketDto)
+        {
+            var result = await serviceManager.BasketService.UpdateBasketAsync(basketDto);
+            return Ok(result);
+        }
+
+        [HttpDelete] // DELETE: /api/baskets?id
+        public async Task<IActionResult> Delete(string id)
+        {
+            await serviceManager.BasketService.DeleteBasketAsync(id);
+            return NoContent();  // 204
+        }
+    }
+}
